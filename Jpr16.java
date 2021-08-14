3e907cb4a0be55b1cd3837a5378a2d518ff32e88
It's me SANIYA <88643947+saniyamundhe@users.noreply.github.com>
2021-08-14 12:00:29 +0530

    Create Jpr16.java

---
+++ Jpr16.java
@@ -0,0 +1,9 @@
+public class JPR16
+{
+ public static void main(String[] args) {
+ float GST ;
+ int basePrice=34900;
+ GST= basePrice*(18.0f/100);
+ System.out.println("Rupees you have to pay :"+GST+" GST amount on RS. 34900");
+}
+} 
