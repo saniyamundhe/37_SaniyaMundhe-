26475036619e52ce51ff440ac9e5ff03fd7c2f98
It's me SANIYA <88643947+saniyamundhe@users.noreply.github.com>
2021-08-14 11:47:07 +0530

    Create Jpr11.java

---
+++ Jpr11.java
@@ -0,0 +1,15 @@
+public class JPR11
+
+{  
+public static void main(String[] args)   
+{  
+
+char ch1 = 'X';  
+char ch2 = 'x';  
+// variable that stores the integer value of the character  
+int asciivalue1 = ch1;  
+int asciivalue2 = ch2;  
+System.out.println("The ASCII value of " + ch1 + " is: " + asciivalue1);  
+System.out.println("The ASCII value of " + ch2 + " is: " + asciivalue2);  
+}  
+}  
