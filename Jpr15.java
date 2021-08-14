4020b1c890fa77266ff037baa25af54ba70f0d11
It's me SANIYA <88643947+saniyamundhe@users.noreply.github.com>
2021-08-14 11:55:51 +0530

    Create Jpr15.java

---
+++ Jpr15.java
@@ -0,0 +1,13 @@
+public class JPR15 {
+
+    public static void main(String[] args) {
+
+        char ch = 's';
+
+        if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' )
+            System.out.println(ch + " is vowel");
+        else
+            System.out.println(ch + " is consonant");
+
+    }
+}
