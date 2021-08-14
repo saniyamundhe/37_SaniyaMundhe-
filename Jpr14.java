25223e4c1f8fc41c6014b14e9697011899e8137a
It's me SANIYA <88643947+saniyamundhe@users.noreply.github.com>
2021-08-14 11:51:27 +0530

    Create Jpr14.java

---
+++ Jpr14.java
@@ -0,0 +1,13 @@
+public class JPR14{  
+public static void main(String[] args) {  
+    //defining a variable  
+    int a[]={88,37,1658};
+    for(int i=0;i<a.length;i++)  
+    //Check if the number is divisible by 2 or not  
+    if(a[i]%2==0){  
+        System.out.println("even number");  
+    }else{  
+        System.out.println("odd number");  
+    }  
+}  
+}  
