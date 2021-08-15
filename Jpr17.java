fb98fff69e59d900f773c9d2eab0de92d0a99c2c
It's me SANIYA <88643947+saniyamundhe@users.noreply.github.com>
2021-08-15 18:02:13 +0530

    Update Jpr17.java

--- Jpr17.java
+++ Jpr17.java
@@ -1,16 +1,23 @@
+public class JPR17
+ {
 
-public class JPR17 {
-    public static void main(String[] args) {
-        int amount=161258;
-        float si,ci;
-        //simple interest calculating
-        si=((amount*0.05f*1)/100)/12;
-        System.out.println("Monthly Simple interest on amount "+amount+" is: "+si);
-        //compund interest calculating
-        float rate=(1+(5.0f/100));
-        ci=(amount*rate-amount)/12;
-        System.out.println("Monthly Simple interest on amount "+amount+" is: "+ci);
+public static void main(String[] args)
+ {
 
+int p=161258; // principle
+Float r=0.05f;//rate of interest
+int n=3; // time 
+double si, ci ;
 
-    }
+//Simple interest=(p*r*t)/(100*12)
+
+si=(p*r*n)/(100/12);
+System.out.println(" simple interest will be:" +si);
+
+//compound Interest =p*((1+ 0.05 / 12)^12) (12 *1) -p
+
+ci=p*Math.pow(1-(r/12), 12)-p;
+System.out.println("compound interest will be:"
++ci);
+}
 }
