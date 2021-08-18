d5c2d47656bc64927fecfa5002cc8070ca1b2fad
It's me SANIYA <88643947+saniyamundhe@users.noreply.github.com>
2021-08-18 10:22:13 +0530

    Create Javaass3.java

---
+++ Javaass3.java
@@ -0,0 +1,12 @@
+public class ass3
+{
+public static void main (String [] args)
+{
+ float avg_salofthreeemployees=1000.0f;
+int salary_e1=1100;
+int salary_e2=500;
+float salary_e3;
+ salary_e3=(float)((avg_salofthreeemployees*3.0)-(salary_e1+salary_e2)); 
+System.out.println("third employee earn salary: " + salary_e3);
+} 
+} 
