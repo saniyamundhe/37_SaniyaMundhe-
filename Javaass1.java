02660617c9ee92064dd65bd77b719771bf73010d
It's me SANIYA <88643947+saniyamundhe@users.noreply.github.com>
2021-08-18 14:27:26 +0530

    Update Javaass1.java

--- Javaass1.java
+++ Javaass1.java
@@ -1,11 +1,11 @@
-public class ass1
-{
-   public static void main(String[] args)
-    {
-      String string ="Java Skills";
-      String reverse = new 
-      StringBuffer(string).reverse().toString();
-      System.out.println("\nString before reverse: "+string);
-      System.out.println("String after reverse: "+reverse);
-   }
-} 
+public class Ass1
+ {
+  public static void main(String[] args)
+  {
+    String s="JavaSkills";
+      for(int i=s.length()-1; i>=0; i--)
+     {
+       System.out.print(s.charAt(i));
+      }
+   }  
+}  
