09c3cbdf4e95b2b039ef2b16145cb4c4d8b0bc63
It's me SANIYA <88643947+saniyamundhe@users.noreply.github.com>
2021-08-18 10:22:35 +0530

    Create Javaass4.java

---
+++ Javaass4.java
@@ -0,0 +1,13 @@
+public class ass4
+{
+	public static void main(String[] args) 
+	{
+		int num=30;
+		int den=100;
+		//Reduce the Fraction by taking common factor of the numerator and denominator 
+		int commonfactor =10;
+		int nume=num/commonfactor ;
+		int deno=den/commonfactor ;
+		System.out.println("The percentage to Fraction of 30% is "+ nume+ "/" + deno);	
+	}
+}
